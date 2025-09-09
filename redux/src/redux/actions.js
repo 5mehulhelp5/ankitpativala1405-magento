40fe3e1a0b1382529798c3df3beee70cfee65f1b
ankitpativala1405 <ankitj1405@gmail.com>
2025-09-09 09:09:25 +0530

    update

--- redux/src/redux/actions.js
+++ redux/src/redux/actions.js
@@ -6,3 +6,6 @@ const counterActions = {
 }
 
 export default counterActions
+
+
+
--- redux/src/redux/counterreducer.js
+++ redux/src/redux/counterreducer.js
@@ -16,3 +16,6 @@ function counterReducer(state = initialState, action) {
 }
 
 export default counterReducer;
+
+
+
